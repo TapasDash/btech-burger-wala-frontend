b96ca4837201470d4db2754f8607b16e203e4066
TapasDash <tapasdash017@gmail.com>
2023-01-20 04:47:05 +0530

    up until the main page

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,21 @@
+import "../../styles/footer.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      {/* <div> */}
+      <h2>Btech Burger Wala</h2>
+      <p>
+        We are serving delicious, freshly made burgers recently. We use only the
+        highest quality ingredients and pride ourselves on our commitment to
+        customer satisfaction
+      </p>
+      <br />
+      <em>We give attention to geniune feedback!</em>
+      <strong>Copyright &copy; 2023 @btechburgerwala</strong>
+      {/* </div> */}
+    </footer>
+  );
+};
+
+export default Footer;
